41a746873e4cfd0a7c913ac2cb1013572a3519c1
Prasann Viswanathan <62383938+iamprasann@users.noreply.github.com>
2021-11-18 23:01:43 +0530

    Mukti de de bhagwaan

---
+++ EE340/Week 9/top_block.py
@@ -0,0 +1,308 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# GNU Radio version: v3.8.2.0-57-gd71cd177
+
+from distutils.version import StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+from gnuradio import analog
+from gnuradio import blocks
+import pmt
+from gnuradio import digital
+from gnuradio import filter
+from gnuradio.filter import firdes
+from gnuradio import gr
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio.filter import pfb
+from math import pi
+
+from gnuradio import qtgui
+
+class top_block(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Top Block")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Top Block")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.sps = sps = 3
+        self.nfilts = nfilts = 32
+        self.ntaps = ntaps = 11*nfilts*sps
+        self.excess_bw = excess_bw = 0.4
+        self.tx_taps = tx_taps = firdes.root_raised_cosine(nfilts, nfilts, 1.0, excess_bw, ntaps)
+        self.timing_bw = timing_bw = 2*pi/100
+        self.samp_rate_0 = samp_rate_0 = 20000*sps
+        self.samp_rate = samp_rate = 20000
+        self.rx_taps = rx_taps = firdes.root_raised_cosine(nfilts, nfilts, 1.0, excess_bw, ntaps)
+        self.freq_bw = freq_bw = 2*pi/100
+        self.fll_ntaps = fll_ntaps = 55
+        self.const_points = const_points = 4
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.rational_resampler_xxx_0_0 = filter.rational_resampler_ccf(
+                interpolation=1,
+                decimation=10,
+                taps=None,
+                fractional_bw=None)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=10,
+                decimation=1,
+                taps=None,
+                fractional_bw=None)
+        self.pfb_arb_resampler_xxx_0 = pfb.arb_resampler_ccf(
+            sps,
+            taps=tx_taps,
+            flt_size=32)
+        self.pfb_arb_resampler_xxx_0.declare_sample_delay(0)
+        self.low_pass_filter_0_0 = filter.fir_filter_fff(
+            1,
+            firdes.low_pass(
+                1,
+                500e3,
+                50e3,
+                10e3,
+                firdes.WIN_HAMMING,
+                6.76))
+        self.low_pass_filter_0 = filter.fir_filter_fff(
+            1,
+            firdes.low_pass(
+                1,
+                500e3,
+                50e3,
+                10e3,
+                firdes.WIN_HAMMING,
+                6.76))
+        self.digital_pfb_clock_sync_xxx_0 = digital.pfb_clock_sync_ccf(sps, timing_bw, rx_taps, nfilts, 16, 1.5, 1)
+        self.digital_diff_encoder_bb_0 = digital.diff_encoder_bb(2)
+        self.digital_diff_decoder_bb_0 = digital.diff_decoder_bb(2)
+        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bc((-1,1), 1)
+        self.blocks_unpack_k_bits_bb_0 = blocks.unpack_k_bits_bb(8)
+        self.blocks_throttle_0_0 = blocks.throttle(gr.sizeof_gr_complex*1, 20000,True)
+        self.blocks_threshold_ff_0 = blocks.threshold_ff(-0.001, 0.001, 0)
+        self.blocks_skiphead_0 = blocks.skiphead(gr.sizeof_char*1, 31)
+        self.blocks_pack_k_bits_bb_0 = blocks.pack_k_bits_bb(8)
+        self.blocks_multiply_xx_0_1 = blocks.multiply_vff(1)
+        self.blocks_multiply_xx_0_0 = blocks.multiply_vff(1)
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
+        self.blocks_float_to_uchar_0 = blocks.float_to_uchar()
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, 'C:\\Users\\HOME\\Desktop\\SEM 5\\EE340 - Comms. Lab\\Lab 9\\Original_Text.txt', False, 0, 0)
+        self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, 'C:\\Users\\HOME\\Desktop\\SEM 5\\EE340 - Comms. Lab\\Lab 9\\task2-text-op.txt', False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+        self.blocks_complex_to_real_0_0 = blocks.complex_to_real(1)
+        self.blocks_complex_to_real_0 = blocks.complex_to_real(1)
+        self.analog_sig_source_x_0_1 = analog.sig_source_f(500e3, analog.GR_COS_WAVE, 100e3, 1, 0, 0)
+        self.analog_sig_source_x_0_0 = analog.sig_source_f(500e3, analog.GR_SIN_WAVE, 100e3, -1, 0, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_c(500e3, analog.GR_COS_WAVE, 100e3, 1, 0, 0)
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0_0, 1))
+        self.connect((self.analog_sig_source_x_0_1, 0), (self.blocks_multiply_xx_0_1, 1))
+        self.connect((self.blocks_complex_to_real_0, 0), (self.blocks_multiply_xx_0_0, 0))
+        self.connect((self.blocks_complex_to_real_0, 0), (self.blocks_multiply_xx_0_1, 0))
+        self.connect((self.blocks_complex_to_real_0_0, 0), (self.blocks_threshold_ff_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_unpack_k_bits_bb_0, 0))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.rational_resampler_xxx_0_0, 0))
+        self.connect((self.blocks_float_to_uchar_0, 0), (self.digital_diff_decoder_bb_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_complex_to_real_0, 0))
+        self.connect((self.blocks_multiply_xx_0_0, 0), (self.low_pass_filter_0_0, 0))
+        self.connect((self.blocks_multiply_xx_0_1, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.blocks_pack_k_bits_bb_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.blocks_skiphead_0, 0), (self.blocks_pack_k_bits_bb_0, 0))
+        self.connect((self.blocks_threshold_ff_0, 0), (self.blocks_float_to_uchar_0, 0))
+        self.connect((self.blocks_throttle_0_0, 0), (self.blocks_complex_to_real_0_0, 0))
+        self.connect((self.blocks_unpack_k_bits_bb_0, 0), (self.digital_diff_encoder_bb_0, 0))
+        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.pfb_arb_resampler_xxx_0, 0))
+        self.connect((self.digital_diff_decoder_bb_0, 0), (self.blocks_skiphead_0, 0))
+        self.connect((self.digital_diff_encoder_bb_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))
+        self.connect((self.digital_pfb_clock_sync_xxx_0, 0), (self.blocks_throttle_0_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.low_pass_filter_0_0, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.pfb_arb_resampler_xxx_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.rational_resampler_xxx_0_0, 0), (self.digital_pfb_clock_sync_xxx_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_sps(self):
+        return self.sps
+
+    def set_sps(self, sps):
+        self.sps = sps
+        self.set_ntaps(11*self.nfilts*self.sps)
+        self.set_samp_rate_0(20000*self.sps)
+        self.pfb_arb_resampler_xxx_0.set_rate(self.sps)
+
+    def get_nfilts(self):
+        return self.nfilts
+
+    def set_nfilts(self, nfilts):
+        self.nfilts = nfilts
+        self.set_ntaps(11*self.nfilts*self.sps)
+        self.set_rx_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0, self.excess_bw, self.ntaps))
+        self.set_tx_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0, self.excess_bw, self.ntaps))
+
+    def get_ntaps(self):
+        return self.ntaps
+
+    def set_ntaps(self, ntaps):
+        self.ntaps = ntaps
+        self.set_rx_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0, self.excess_bw, self.ntaps))
+        self.set_tx_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0, self.excess_bw, self.ntaps))
+
+    def get_excess_bw(self):
+        return self.excess_bw
+
+    def set_excess_bw(self, excess_bw):
+        self.excess_bw = excess_bw
+        self.set_rx_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0, self.excess_bw, self.ntaps))
+        self.set_tx_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0, self.excess_bw, self.ntaps))
+
+    def get_tx_taps(self):
+        return self.tx_taps
+
+    def set_tx_taps(self, tx_taps):
+        self.tx_taps = tx_taps
+        self.pfb_arb_resampler_xxx_0.set_taps(self.tx_taps)
+
+    def get_timing_bw(self):
+        return self.timing_bw
+
+    def set_timing_bw(self, timing_bw):
+        self.timing_bw = timing_bw
+        self.digital_pfb_clock_sync_xxx_0.set_loop_bandwidth(self.timing_bw)
+
+    def get_samp_rate_0(self):
+        return self.samp_rate_0
+
+    def set_samp_rate_0(self, samp_rate_0):
+        self.samp_rate_0 = samp_rate_0
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+
+    def get_rx_taps(self):
+        return self.rx_taps
+
+    def set_rx_taps(self, rx_taps):
+        self.rx_taps = rx_taps
+        self.digital_pfb_clock_sync_xxx_0.update_taps(self.rx_taps)
+
+    def get_freq_bw(self):
+        return self.freq_bw
+
+    def set_freq_bw(self, freq_bw):
+        self.freq_bw = freq_bw
+
+    def get_fll_ntaps(self):
+        return self.fll_ntaps
+
+    def set_fll_ntaps(self, fll_ntaps):
+        self.fll_ntaps = fll_ntaps
+
+    def get_const_points(self):
+        return self.const_points
+
+    def set_const_points(self, const_points):
+        self.const_points = const_points
+
+
+
+
+
+def main(top_block_cls=top_block, options=None):
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+
+    qapp.aboutToQuit.connect(quitting)
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
